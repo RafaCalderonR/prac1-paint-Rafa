854d73d357c0dd3232d62bce80068bc1a3c06b20
Rafa <wirecobweb@gmail.com>
2019-10-13 18:14:44 +0200

    first steps

---
+++ src/controllers/canvas.controller.ts
@@ -0,0 +1,8 @@
+
+
+export class CanvasController{
+
+    DrawSquare=(Parameters: Square)=>{
+
+    }
+}
---
+++ src/models/shapes/square.ts
@@ -0,0 +1,6 @@
+export interface ISquare{
+    context: CanvasRenderingContext2D,
+    axisX: number,
+    axisY: number,
+    size: number
+}
