634e7566e8037f0935e73e79b75da737e2b3c095
Rafa <wirecobweb@gmail.com>
2019-10-15 19:22:19 +0200

    love of lesbian tiene 2 canciones buenas

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,5 @@ import { ShapeModel } from './models/shape.model';
 const view: Document = window.document;
 
 const controller = new ShapeModel("square", 0, 0 , 100, "#C0C0C0");
-const pepe: CanvasView = new CanvasView(window.document, controller);
+const canvasView: CanvasView = new CanvasView(window.document, controller);
 
